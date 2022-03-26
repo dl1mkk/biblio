93bd9c892ad484db461cb0d0cf9b40b127140a1b
Veit Heise, DL1MKK <52625234+dl1mkk@users.noreply.github.com>
2022-03-26 14:12:49 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# zend
+ZEND 2.3.0
+## Beschreibung
+Dies ist eine HTML5-App zur Verwaltung einer umfangreichen Bibliothek auf einem Apache2-Server.
+
---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+//Disclaimer of Copyrights:
+//(c)2018 by Veit Heise, DK2VH, 63477 Maintal, Germany
+//All rights depends to Zend-Technologies and IonCube Ltd. for PHP-Coding
+//All rights depends to Veit Heise, PQM-Consulting, 63477 Maintal for PHP and HTML-User Code
+//All rights depends to W3C for HTML and CSS and CSS2
+// (C)2019 BY VEIT HEISE, PQM-CONSULTING, MAINTAL
+// PROJECT: BIBLIOTHEK PIER CARLO RABINO, 60388 FRANKFURT AM MAIN
+// THIS PROJECT IS HANDLED UNDER PRIVATE DOMAIN LICENCE
+// COPY AND DISTRIBUTION WITHOUT PERMISSION OF THE AUTHOR VEIT HEISE IS NOT PERMITTED
+// SERVICE NOTICES PLEASE SENT TO veit.heise@heise-media.eu//
+//
+//
+?>
+<h4>Disclaimer</h4>
+<div>
+  Impressum <a href="impressum.php">Veit Heise</a>.
+</div>
+<p class="footer">&copy;2021,2020,2019 by <a href="impressum.php">Veit Heise, PQM Consulting Maintal</a>, Germany<br/>
+Alle Rechte vorberhalten. All rights preserved.</p>
