eade1e35efcf8c8030fe2f1e64cd7089fe8a95ec
Veit Heise, DL1MKK <52625234+dl1mkk@users.noreply.github.com>
2022-03-29 03:41:56 +0200

    Add files via upload

--- py/php/adm-rollout-p00.php
+++ py/php/adm-rollout-p00.php
@@ -1,10 +1,4 @@
 <?php
     //
-    $ftp = ftp_connect('w00b6ff8.kasserver.com');
-    if (!$res=ftp_login($ftp,'f014b242','QTWAEVbXFbuzuGq6'))
-        die('Login failed.');
-    if (!$res=ftp_get($ftp,'/tmp/zend-install.tgz','zend-install.tgz'))
-        die('Get failed.');  
-    exit;
     //
 ?>
